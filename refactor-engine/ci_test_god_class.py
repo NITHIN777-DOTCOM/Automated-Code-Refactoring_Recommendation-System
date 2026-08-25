c96a95d033ddddd5392bd5df34dac26a5a37d58c
Nithin <nithin.r2024a@vitstudent.ac.in>
2026-08-25 05:52:29 +0530

    test: add code smell for CI scan

---
+++ refactor-engine/ci_test_god_class.py
@@ -0,0 +1,49 @@
+class GodClass:
+    def __init__(self):
+        self.users = []
+        self.orders = []
+        self.products = []
+        self.payments = []
+        self.notifications = []
+
+    def create_user(self):
+        pass
+
+    def delete_user(self):
+        pass
+
+    def update_user(self):
+        pass
+
+    def create_order(self):
+        pass
+
+    def cancel_order(self):
+        pass
+
+    def calculate_order(self):
+        pass
+
+    def add_product(self):
+        pass
+
+    def remove_product(self):
+        pass
+
+    def update_product(self):
+        pass
+
+    def process_payment(self):
+        pass
+
+    def refund_payment(self):
+        pass
+
+    def send_email(self):
+        pass
+
+    def send_sms(self):
+        pass
+
+    def send_notification(self):
+        pass
